0c14314aa295b29ea201dff9883127504c54dd12
timcrepinpro <tim.crepin@edouardgand.fr>
2026-03-23 15:52:34 +0100

    creation classe personnage

---
+++ Personnage.cs
@@ -0,0 +1,26 @@
+class Personnage
+{
+    protected string nom;
+    protected int vie;
+
+
+    public Personnage(string nom, int vie)
+    {
+        this.nom = nom;
+        this.vie = vie;
+    }
+
+    public void RecevoirDegat(int degats)
+    {
+        vie -= degats;
+        if (vie < 0)
+        {
+            vie = 0;
+        }
+    }
+
+    public void afficher()
+    {
+        Console.WriteLine($"Nom: {nom}, Vie: {vie}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace pooclass;
+﻿namespace program;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+
+        Personnage p1 = new Personnage("Jean eude", 100);
+        p1.afficher();
+        p1.RecevoirDegat(20);
+        p1.afficher();
     }
 }
